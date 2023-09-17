203ce5f4c6adbe494cd1138a11373a254f869131
Alliyah Joyce Sarip <saripalliyahjoyce@gmail.com>
2023-09-17 19:00:23 +0800

    refactor: remove unused http methods

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
-    allow_methods=["GET", "POST", "PUT", "DELETE"],
+    allow_methods=["GET"],
     allow_headers=["Content-Type"],
 )
 
